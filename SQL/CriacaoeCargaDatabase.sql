83bbf734849fbf9b6fc63f49cec24bd775574ba8
brunomsk9 <brunomsk9@gmail.com>
2022-08-07 14:09:59 -0300

    Criação da Primeira Task!

---
+++ SQL/CriacaoeCargaDatabase.sql
@@ -0,0 +1,50 @@
+-- Selecionando Master para Deletar Banco Criado --
+USE master
+GO
+
+-- Verifica se a Tabela, se existir exclui -- 
+DROP TABLE IF EXISTS [PRJIntegration].[Projeto].[Clientes]
+GO
+
+
+-- Excluindo Banco --
+DROP DATABASE IF EXISTS PRJIntegration
+GO
+
+
+
+-- Criando Banco de Dados --
+CREATE DATABASE PRJIntegration
+GO
+
+-- Escolhendo Banco --
+Use PRJIntegration
+GO
+
+-- Criando Schema
+Create Schema Projeto
+GO
+
+
+-- Criando Tabela --
+CREATE TABLE Projeto.Clientes (
+	IdClientes INT IDENTITY PRIMARY KEY
+	,Nome Varchar(100)
+	,Sexo Char(1)
+	,Email Varchar(100)
+)
+GO
+
+
+-- Inserindo Dados -- 
+Insert Into Projeto.Clientes Values('Clara','F','clara@gmail.com')
+Insert Into Projeto.Clientes Values('Mafra','M','mafra@gmail.com')
+Insert Into Projeto.Clientes Values('João','F','joao@gmail.com')
+Insert Into Projeto.Clientes Values('Ana','F','ana@gmail.com')
+Insert Into Projeto.Clientes Values('Lilian','F','lilian@gmail.com')
+GO
+
+-- Recuperando Informações Inseridas --
+SELECT *
+FROM Projeto.Clientes
+GO
